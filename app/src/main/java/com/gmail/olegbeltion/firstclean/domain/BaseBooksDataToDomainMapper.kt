e38d8bb5374526b97012b72164a17c08fc45d30b
Oleg Suchkov <olegbeltion@gmail.com>
2021-11-12 21:23:07 +0300

    Fix domain mapping

--- app/src/main/java/com/gmail/olegbeltion/firstclean/domain/BaseBooksDataToDomainMapper.kt
+++ app/src/main/java/com/gmail/olegbeltion/firstclean/domain/BaseBooksDataToDomainMapper.kt
@@ -3,9 +3,34 @@ package com.gmail.olegbeltion.firstclean.domain
 import com.gmail.olegbeltion.firstclean.data.BookData
 import com.gmail.olegbeltion.firstclean.data.BookDataToDomainMapper
 import com.gmail.olegbeltion.firstclean.data.BooksDataToDomainMapper
+import com.gmail.olegbeltion.firstclean.data.TestamentTemp
+import retrofit2.HttpException
+import java.net.UnknownHostException
 
 class BaseBooksDataToDomainMapper(private val mapper: BookDataToDomainMapper) :
     BooksDataToDomainMapper {
-    override fun map(books: List<BookData>) = BooksDomain.Success(books, mapper)
-    override fun map(e: Exception) = BooksDomain.Fail(e)
+    override fun map(books: List<BookData>): BooksDomain.Success {
+
+        val data = mutableListOf<BookDomain>()
+        val temp = TestamentTemp.Base()
+        books.forEach { bookData ->
+            if (!bookData.matches(temp)) {
+                if (temp.isEmpty())
+                    data.add(BookDomain.Testament(TestamentType.OLD))
+                else
+                    data.add(BookDomain.Testament(TestamentType.NEW))
+                bookData.saveTestament(temp)
+            }
+            data.add(bookData.map(mapper))
+        }
+        return BooksDomain.Success(data)
+    }
+
+    override fun map(e: Exception) = BooksDomain.Fail(
+        when (e) {
+            is UnknownHostException -> ErrorType.NO_CONNECTION
+            is HttpException -> ErrorType.SERVICE_UNAVAILABLE
+            else -> ErrorType.GENERIC_ERROR
+        }
+    )
 }
--- app/src/main/java/com/gmail/olegbeltion/firstclean/domain/BooksDomain.kt
+++ app/src/main/java/com/gmail/olegbeltion/firstclean/domain/BooksDomain.kt
@@ -9,40 +9,14 @@ import retrofit2.HttpException
 import java.net.UnknownHostException
 
 sealed class BooksDomain : Abstract.Object<BooksUi, BooksDomainToUiMapper> {
-    class Success(
-        private val books: List<BookData>,
-        private val bookMapper: BookDataToDomainMapper
+    data class Success(
+        private val books: List<BookDomain>
     ) : BooksDomain() {
-        override fun map(mapper: BooksDomainToUiMapper): BooksUi {
-            val data = mutableListOf<BookDomain>()
-            val ot = "OT"
-            val nt = "NT"
-            val temp = TestamentTemp.Base()
-            books.forEach { bookData ->
-                if (!bookData.matches(temp)) {
-                    if (temp.isEmpty())
-                        data.add(BookDomain.Testament(TestamentType.OLD))
-                    else
-                        data.add(BookDomain.Testament(TestamentType.NEW))
-                    bookData.saveTestament(temp)
-                }
-                data.add(bookData.map(bookMapper))
-            }
-
-
-
-            return mapper.map(data)
-        }
+        override fun map(mapper: BooksDomainToUiMapper) =  mapper.map(books)
     }
 
-    class Fail(private val e: Exception) : BooksDomain() {
-        override fun map(mapper: BooksDomainToUiMapper) = mapper.map(
-            when (e) {
-                is UnknownHostException -> ErrorType.NO_CONNECTION
-                is HttpException -> ErrorType.SERVICE_UNAVAILABLE
-                else -> ErrorType.GENERIC_ERROR
-            }
-        )
+    data class Fail(private val e: ErrorType) : BooksDomain() {
+        override fun map(mapper: BooksDomainToUiMapper) = mapper.map(e)
     }
 
 }
